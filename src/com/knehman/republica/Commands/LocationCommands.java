71a4d0ac5ef5efc64c1ce20d09d281cb7a9330e7
Kyle Nehman <nehman1@umbc.edu>
2017-08-29 02:55:17 -0400

    Fixed package names

---
+++ src/com/knehman/republica/Commands/LocationCommands.java
@@ -0,0 +1,56 @@
+package com.knehman.republica.Commands;
+
+import java.util.Arrays;
+import java.util.Locale;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+public class LocationCommands  extends AbstractCommand implements CommandExecutor {
+
+	public LocationCommands(Plugin plugin) {
+		super(LocationCommands.class.getName(), Arrays.asList("loc"));
+		this.plugin = plugin;
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String arg, String[] args) {
+		
+		if (cmd.getName().equalsIgnoreCase("loc")) {
+			if (sender.hasPermission("rsmp.loc") || sender.isOp()) {
+				int argsLength = args.length;
+				Player target = null;
+				
+				if (argsLength == 0) {
+					target = (Player) sender;
+				} else if (argsLength == 1) {
+					target = Bukkit.getPlayer(args[0]);
+				} else {
+					errorMessage(sender, "Invalid amount of arguments");
+					return false;
+				}
+				
+				if (target == null) {
+					errorMessage(sender, "Player isn't online");
+					return false;
+				}
+					
+				Location loc = target.getLocation();
+				String x = String.format(Locale.US, "%.2f", loc.getX());
+				String y = String.format(Locale.US, "%.2f", loc.getY());
+				String z = String.format(Locale.US, "%.2f", loc.getZ());
+				String locString = "X: " + x + ", Y: " + y + ", Z: " + z; 
+				
+				message(sender, target.getName() + "'s location: " + locString);
+				return true;
+			}
+		}
+		return false;
+	}
+}
